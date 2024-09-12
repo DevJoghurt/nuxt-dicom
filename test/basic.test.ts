3c9d91b3997e9e4bd12fca4a82a673dae23050a9
Johannes Hof <johannes.bortz@gmail.com>
2024-09-12 10:26:25 +0200

    fixed test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,14 +1,21 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterAll } from 'vitest'
 import { StoreScu } from '@nuxthealth/node-dicom'
+import { setup, stopServer,  } from '@nuxt/test-utils/e2e'
+import { fileURLToPath } from 'node:url'
 
 describe('StoreSCP', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
+    browser: false
+  });
+
   it('Send file', async () => {
     const storeSCU = new StoreScu({
       addr: '127.0.0.1:1222',
       verbose: false,
-    })
-    storeSCU.addFile('./test/fixtures/basic/files/test.dcm')
-    const result = storeSCU.send()
-    expect(result.status).equals('Success')
-  })
+    });
+    storeSCU.addFile('./test/fixtures/basic/files/test.dcm');
+    const result = storeSCU.send();
+    expect(result.status).equals('Success');
+  });
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,5 @@
-import { fileURLToPath } from 'node:url'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-  environment: 'nuxt',
-  environmentOptions: {
-    nuxt: {
-      rootDir: fileURLToPath(new URL('./test/fixtures/basic', import.meta.url)),
-    },
-  },
+  environment: 'nuxt'
 })
